2868914563017f6248e6dc10d209a912a7de55be
t-east <azmtomya.a127@gmail.com>
2021-10-12 00:17:53 +0900

    ディレクトリ変更

--- src/ethereum/create_contract_address.go
+++ src/ethereum/create_contract_address.go
@@ -12,11 +12,11 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"pairing_test/src/contracts"
+	"pairing_test/src/ethereum/contracts"
 )
 
-const PRIVATE_KEY = "b757df8905a4108376a8cf69dafe7d26aa274d272d7092a0a28f62539ca9d72c"
-const SP_ADDRESS = "0x952DA7F148cEd9c817e05a833B8a6DF8F71abfAC"
+const PRIVATE_KEY = "5f4a70a398a494d4ec34c8fd2e8986c306fc3f37ebe228cb2b273c70fe16ed7f"
+const SP_ADDRESS = "0xeAAA2c6F420aaC6a55821F965d1BD75dc79f38f6"
 
 func main() {
 	client, err := ethclient.Dial("http://127.0.0.1:8545")
@@ -55,7 +55,7 @@ func main() {
 	auth.Nonce = big.NewInt(int64(nonce))
 	auth.Value = big.NewInt(0)      // in wei
 	auth.GasLimit = uint64(3000000) // in units
-	auth.GasPrice = big.NewInt(1000000)
+	auth.GasPrice = big.NewInt(100)
 
 	address, tx, instance, err := contracts.DeployContracts(auth, client, spAddress)
 	if err != nil {
