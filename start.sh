8d4c4f5c86b4ec0873a0ba47f755f38870551221
t-east <azmtomya.a127@gmail.com>
2021-10-19 11:58:57 +0900

    shellファイル削除

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-export MSYS_NO_PATHCONV=1
- 
-docker-compose -f docker-compose.yml down
-
-docker-compose -f docker-compose.yml up -d bn main #user_a user_b user_c sp tpa
-docker ps -a
