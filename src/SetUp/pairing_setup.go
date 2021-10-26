8b9d0bfa4a75464a8966671ff6cec4dcf147f78e
t-east <azmtomya.a127@gmail.com>
2021-10-27 08:57:09 +0900

    setupのハンドラー更新

--- src/SetUp/pairing_setup.go
+++ src/SetUp/pairing_setup.go
@@ -1,33 +1,13 @@
 package SetUp
 
 import (
-	"pairing_test/src/tool"
-
 	"github.com/Nik-U/pbc"
 )
 
-func SetUp(userCount int, paramk1 int, paramk2 int) (tool.Params, []tool.PrivKey) {
-	var para tool.Params
-	// The authority generates system parameters
-	params := pbc.GenerateA(uint32(paramk1), uint32(paramk2))
+func SetUp() (string, []byte, []byte) {
+	params := pbc.GenerateA(uint32(160), uint32(512))
 	pairing := params.NewPairing()
 	g := pairing.NewG1().Rand()
 	u := pairing.NewG1().Rand()
-	para.Pairing = params.String()
-	para.G = g.Bytes()
-	para.U = u.Bytes()
-	// TODO:ブロックチェーンにParaを保存
-	return para
-}
-
-func RegisterUser(id string, para tool.Params) tool.UserKey {
-	var userKey tool.UserKey
-	pairing, _ := pbc.NewPairingFromString(para.Pairing)
-	privKey := pairing.NewZr().Rand()
-	g := pairing.NewG1().SetBytes(para.G)
-	pubKey := pairing.NewG1().MulZn(g, privKey)
-	userKey.ID = id
-	userKey.PubKey = pubKey.Bytes()
-	userKey.PrivKey = privKey.Bytes()
-	return userKey
+	return params.String(), g.Bytes(), u.Bytes()
 }
