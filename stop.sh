c06f153332faf9d51739c9e244f96c2a6686ecf4
t-east <azmtomya.a127@gmail.com>
2021-12-12 17:32:40 +0900

    dockerfile 更新

--- stop.sh
+++ stop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 set -ev
- 
-docker-compose -f docker-compose.yml down
+docker stop gana
+docker rm gana 
+docker rmi -f backend_gana
+# docker-compose -f docker-compose.yml down
 sudo rm -rf ganache/ganache-data
