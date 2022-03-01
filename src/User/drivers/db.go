6981014fa8a4061398a778c0c0e3b543af5898e9
t-east <azmtomya.a127@gmail.com>
2022-03-01 16:37:31 +0900

    gorm方式に変更

--- src/User/drivers/db.go
+++ src/User/drivers/db.go
@@ -1,14 +1,33 @@
 package drivers
+
 import (
-	"github.com/go-sql-driver/mysql"
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
-// TODO: DBの設定をgormにする?
-GetDB() (mysql.Conn, error) {
+
+type SQLHandler struct {
+	Conn *gorm.DB
+}
+
+func GetDB() (*gorm.DB, error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DATABASE"))
-	conn, err := sql.Open("mysql", dsn)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Println(err)
 		return nil, err
 	}
-	return  conn, nil 
+	return  db, nil
+}
+
+func NewSQLHandler() (*SQLHandler, error) {
+	db, err := GetDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sqlHandler := new(SQLHandler)
+	sqlHandler.Conn = db
+	return sqlHandler, nil
 }
