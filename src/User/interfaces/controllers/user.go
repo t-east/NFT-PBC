5a19b30cc895e051656d7ca0da0e029f3bd2ecbb
t-east <azmtomya.a127@gmail.com>
2022-03-01 16:37:56 +0900

    init controllers/user

---
+++ src/User/interfaces/controllers/user.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"net/http"
+	"pairing_test/src/user/interfaces/contracts"
+	"pairing_test/src/user/usecases/port"
+	"gorm.io/gorm"
+)
+
+type UserController struct {
+	// -> gateway.NewUserRepository
+	RepoFactory func(c *gorm.DB) port.UserRepository
+	// // -> contracts.NewUserContracts
+	// ContractFactory func() port.UserContracts
+	// -> crypt.NewUserCrypt
+	CryptFactory func(p contracts.Param) port.UserCrypt
+	// -> presenter.NewUserOutputPort
+	OutputFactory func(w http.ResponseWriter) port.UserOutputPort
+	// -> interactor.NewUserInputPort
+	InputFactory func(
+		o port.UserOutputPort,
+		u port.UserRepository,
+		cr port.UserCrypt,
+		// co port.UserContracts,
+	) port.UserInputPort
+	Param contracts.Param
+	Conn        *gorm.DB
+}
+
+func LoadUserController(db *gorm.DB, param contracts.Param) *UserController {
+	return &UserController{Conn: db, Param: param}
+}
