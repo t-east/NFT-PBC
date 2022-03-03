e62030e87cf34ee0645df5f05f2ba4c76bb9691b
t-east <azmtomya.a127@gmail.com>
2022-03-03 16:52:10 +0900

    inputportの実行の実装

--- src/User/interfaces/controllers/content.go
+++ src/User/interfaces/controllers/content.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
+	"pairing_test/src/user/domains/entities"
 	"pairing_test/src/user/interfaces/contracts"
 	"pairing_test/src/user/usecases/port"
 
@@ -24,10 +26,35 @@ type ContentController struct {
 		cr port.ContentCrypt,
 		// co port.ContentContracts,
 	) port.ContentInputPort
-	Param      contracts.Param
-	SQLHandler *gorm.DB
+	Param contracts.Param
+	Conn  *gorm.DB
 }
 
 func LoadContentController(db *gorm.DB, param contracts.Param) *ContentController {
-	return &ContentController{SQLHandler: db, Param: param}
+	return &ContentController{Conn: db, Param: param}
+}
+
+func (cc *ContentController) Post(w http.ResponseWriter, r *http.Request) {
+	content := &entities.ContentInput{}
+	err := json.NewDecoder(r.Body).Decode(&content)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	outputPort := cc.OutputFactory(w)
+	repository := cc.RepoFactory(cc.Conn)
+	crypt := cc.CryptFactory(cc.Param)
+	inputPort := cc.InputFactory(outputPort, repository, crypt)
+	inputPort.Upload(content)
+}
+
+func (cc *ContentController) Get(w http.ResponseWriter, r *http.Request) {
+	//  TODO: idの取得をちゃんとしたやつにする
+	id := "1"
+
+	outputPort := cc.OutputFactory(w)
+	repository := cc.RepoFactory(cc.Conn)
+	crypt := cc.CryptFactory(cc.Param)
+	inputPort := cc.InputFactory(outputPort, repository, crypt)
+	inputPort.FindByID(id)
 }
