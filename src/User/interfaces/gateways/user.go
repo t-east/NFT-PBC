5dfda5e467547da4132702d5ce8968bca55e2ad6
t-east <azmtomya.a127@gmail.com>
2022-03-03 15:02:18 +0900

    handler 移行

--- src/User/interfaces/gateways/user.go
+++ src/User/interfaces/gateways/user.go
@@ -2,31 +2,50 @@ package gateways
 
 import (
 	"pairing_test/src/user/domains/entities"
-	"pairing_test/src/user/drivers/rdb"
 	"pairing_test/src/user/usecases/port"
 
 	"gorm.io/gorm"
 )
 
-type UserRepository struct {
-	SQLHandler
+type SQLHandler interface {
+	Find(interface{}, ...interface{}) (*entities.User, error)
+	First(interface{}, ...interface{}) (*entities.User, error)
+	Create(interface{}) error
+	Save(interface{}) error
+	Delete(interface{}) *entities.User
+	Where(interface{}, ...interface{}) *entities.User
 }
 
-func NewUserRepository(conn *gorm.DB) port.UserRepository {
-	sqlHandler := &rdb.SQLHandler{Conn: conn}
-	userRepository := UserRepository{sqlHandler}
-	return &userRepository
+type UserRepository struct {
+	Conn *gorm.DB
+}
+
+func NewUserRepository(conn *gorm.DB) *port.UserRepository {
+	return &UserRepository{
+		Conn: conn,
+	}
 }
 
 func (ur *UserRepository) FindByID(id int) (user *entities.User, err error) {
-	userInDB, err := ur.SQLHandler.Find(&user, id)
+	userInDB, err := ur.Handler.Find(&user, id)
 	if err != nil {
 		return nil, err
 	}
-	return user, err
+	return userInDB, nil
 }
 
-func (ur *UserRepository) Create(u *entities.UserCreate) (*entities.User, error) {
-	user, err = ur.SQLHandler.Create(u)
-	return user, err
+func (ur *UserRepository) Create(u *entities.User) (user *entities.User, err error) {
+	err = ur.Handler.Create(u)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (ur *UserRepository) Update(u *entities.User) (user *entities.User, err error) {
+	err = ur.Handler.Save(u)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
